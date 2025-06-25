09e9696597e7c567670ff20a14fd27e52cbb7046
NehaPatel03 <nehapatel3305@gmail.com>
2025-06-26 01:12:59 +0530

    day2 ques 4

---
+++ pattern13.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class pattern13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n = sc.nextInt();
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // if (j == 1 || i == j) {
+        // System.out.print(1);
+        // } else {
+        // System.out.print(" ");
+        // }
+        // }
+        // System.out.println();
+        // }
+        int a[][] = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || i == j) {
+                    a[i][j] = 1;
+                } else {
+                    a[i][j] = a[i - 1][j - 1] + a[i - 1][j];
+                }
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
