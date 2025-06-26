9f9f2ef4f3259a0cecbf8b9bf9afba7d56cf93f0
NehaPatel03 <nehapatel3305@gmail.com>
2025-06-27 00:19:45 +0530

    day3 ques5

---
+++ pattern19.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class pattern19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == (n / 2) + 1 || j == (n / 2) + 1 || i == 1 && j < (n / 2) + 1 || i == n && j > (n / 2) + 1
+                        || j == 1 && i > (n / 2) + 1 || j == n && i < (n / 2) + 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
