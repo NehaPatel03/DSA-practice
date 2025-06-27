65e6881f4e8f7249e1ec54e7a4b9e9e8937604ba
NehaPatel03 <nehapatel3305@gmail.com>
2025-06-27 16:26:57 +0530

    day4 ques2

---
+++ decimalToBase.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class decimalToBase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a decimal number");
+        int d = sc.nextInt();
+        int b = sc.nextInt();
+        int conv = 0;
+        int i = 0;
+        while (d != 0) {
+            int rem = d % b;
+            conv = (int) (conv + rem * Math.pow(10, i));
+            d = d / b;
+            i++;
+        }
+        System.out.println("Converted number is:" + conv);
+        sc.close();
+    }
+}
